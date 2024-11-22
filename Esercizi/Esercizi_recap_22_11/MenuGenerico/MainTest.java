26c273ccbf4d478fa68f556d4d1ae79321204a7b
Lorenzo Pergamo <periclepergamo99@gmail.com>
2024-11-22 15:09:07 +0100

    update Menu

--- Esercizi/Esercizi_recap_22_11/MenuGenerico/MainTest.java
+++ Esercizi/Esercizi_recap_22_11/MenuGenerico/MainTest.java
@@ -2,8 +2,8 @@ package Esercizi_recap_22_11.MenuGenerico;
 
 public class MainTest {
 
-    public static AddUserHandler addUserHandler = new AddUserHandler();
-    public static PrintUsersHandler printUsersHandler = new PrintUsersHandler();
+    public static UsersHandler addUserHandler = new AddUserHandler();
+    public static UsersHandler printUsersHandler = new PrintUsersHandler();
     public static Menu homeMenu = Menu.getInstance();
     public static void main(String[] args) {
         homeMenu.addHandler(addUserHandler);
